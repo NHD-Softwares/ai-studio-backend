a8b93f18539e0b045b0974275ddfddcbb7a39222
Nour El Habbal <nour.elhabbal9@gmail.com>
2026-09-14 18:02:30 +0300

    feat: Add Better Auth and configure it

---
+++ src/lib/auth.ts
@@ -0,0 +1,13 @@
+import { betterAuth } from 'better-auth';
+import { prismaAdapter } from 'better-auth/adapters/prisma';
+
+import { prisma } from './prisma.js';
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: 'postgresql', // or "mysql", "sqlite", ...etc
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+});
